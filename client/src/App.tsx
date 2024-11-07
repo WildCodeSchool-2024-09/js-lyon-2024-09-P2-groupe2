2cd3fee5e475df73bbdca6ed41efc14c2e1c8bce
Angelica Lazaro <angelicamld@gmail.com>
2024-11-07 11:19:58 +0100

    premier pull request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,40 @@
 import vangoghImg from "./assets/images/vangogh.jpg";
-import "./App.css"; 
+import "./App.css";
 
 function App() {
   return (
     <>
-      <header> 
+      <header>
         <nav>
           <ul>
             <li>Home</li>
             <li>Favoris</li>
             <li>À propos</li>
           </ul>
-       </nav>
-       <h1>WeArt</h1>
-       <input/> Rechercher
+        </nav>
+        <h1>WeArt</h1>
+        <input /> Rechercher
       </header>
       <main>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
-        <img className="vgImg" src={vangoghImg} alt='vangogh'/>
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" />
       </main>
 
       <footer>
-        <p>Disclaimer : <br/> Les œuvres et images sont présentées à des fins éducatives. Les droits d'auteur appartiennent à leurs détenteurs. Nous ne garantissons pas l'exactitude des informations.</p>
+        <p>
+          Disclaimer : <br /> Les œuvres et images sont présentées à des fins
+          éducatives. Les droits d'auteur appartiennent à leurs détenteurs. Nous
+          ne garantissons pas l'exactitude des informations.
+        </p>
         <p>Copyright 2024 ©</p>
-
       </footer>
     </>
   );
