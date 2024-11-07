15c8d592824dc36acded13c7b9b3c5fd4ccfd61f
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-11-07 14:39:01 +0100

    S01US07_Barre_de_recherche_Done_Nadir

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import vangoghImg from "./assets/images/vangogh.jpg";
 import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
@@ -13,9 +13,10 @@ function App() {
           </ul>
         </nav>
         <h1>WeArt</h1>
-        <input /> Rechercher
+        <SearchBar />
       </header>
       <main>
+        {/* <img className="vgImg" src={vangoghImg} alt="vangogh" />
         <img className="vgImg" src={vangoghImg} alt="vangogh" />
         <img className="vgImg" src={vangoghImg} alt="vangogh" />
         <img className="vgImg" src={vangoghImg} alt="vangogh" />
@@ -24,8 +25,7 @@ function App() {
         <img className="vgImg" src={vangoghImg} alt="vangogh" />
         <img className="vgImg" src={vangoghImg} alt="vangogh" />
         <img className="vgImg" src={vangoghImg} alt="vangogh" />
-        <img className="vgImg" src={vangoghImg} alt="vangogh" />
-        <img className="vgImg" src={vangoghImg} alt="vangogh" />
+        <img className="vgImg" src={vangoghImg} alt="vangogh" /> */}
       </main>
 
       <footer>
