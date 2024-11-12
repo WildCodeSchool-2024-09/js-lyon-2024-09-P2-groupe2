1beba4394d10f3b96b25321683e0ad0825e4aef7
Nadir AMMISAID <155676747+nadir-ammisaid@users.noreply.github.com>
2024-11-12 12:29:20 +0100

    Revert "Création du composant CardArt.tsx et CardArt.css"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
+import basqImg from "./assets/images/basquiat.jpg";
+import fridaImg from "./assets/images/frida.jpg";
+import klimtImg from "./assets/images/klimt.jpg";
+import munchImg from "./assets/images/lecri.jpg";
+import terrasseImg from "./assets/images/terrasse.jpg";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import CardArt from "./components/CardArt";
 import Logo from "./components/Logo";
 import SearchBar from "./components/SearchBar";
 import Footer from "./components/footer";
@@ -10,20 +14,20 @@ function App() {
     <>
       <header>
         <Navbar />
+        {/* <h1>WeArt</h1> */}
         <Logo />
         <SearchBar />
       </header>
       <main>
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
-        <CardArt />
+        <img className="basquiatImg" src={basqImg} alt="peinture de Basquiat" />
+        <img className="fridaImg" src={fridaImg} alt="peinture de Frida" />
+        <img className="klimtImg" src={klimtImg} alt="peinture de Klimt" />
+        <img className="munchImg" src={munchImg} alt="peinture de Munch" />
+        <img
+          className="terrasseImg"
+          src={terrasseImg}
+          alt="peinture de Vangogh"
+        />
       </main>
 
       <footer>
--- client/src/components/CardArt.tsx
+++
@@ -1,9 +0,0 @@
-import basqImg from "../assets/images/basquiat.jpg";
-import "./CardArt.css";
-function CardArt() {
-  return (
-    <img className="basquiatImg" src={basqImg} alt="peinture de Basquiat" />
-  );
-}
-
-export default CardArt;
