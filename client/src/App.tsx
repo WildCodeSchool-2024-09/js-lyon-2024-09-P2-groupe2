4d2168514e4ab7914c6e1c49fc41228cb77a85a8
Nadir AMMISAID <nadir.ammisaid@outlook.com>
2024-11-13 12:13:09 +0100

    Structuration du Footer Finale

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import CardArt from "./components/CardArt";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
