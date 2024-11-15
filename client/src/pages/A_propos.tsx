572c129d05a7ed35eea1b3156e7818fce96da707
Angelica Lazaro <angelicamld@gmail.com>
2024-11-15 09:50:23 +0100

    Page a propos avec image, titre et sous-titre

--- client/src/pages/A_propos.tsx
+++ client/src/pages/A_propos.tsx
@@ -1,16 +1,26 @@
-import basqImg from "../assets/images/basquiat.jpg";
+import "./A_propos.css";
+import dreamTeam from "../assets/images/dreamteam.jpg";
 function A_Propos() {
   return (
     <>
-      <img className="basquiatImg" src={basqImg} alt="peinture de Basquiat" />
-      <h1>Bienvenue à la page À propos</h1>
-      <h2>Our dream team</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-        necessitatibus quibusdam voluptatum eum, maxime voluptatibus assumenda
-        facere animi dolorum est sunt, totam aspernatur quaerat rem similique
-        officia soluta id magnam.
-      </p>
+      <section className="dreamTeamSection">
+        <img className="dreamTeam" src={dreamTeam} alt="photo-d'equipe" />
+        <br />
+        <h1 className="titre-a-propos">Our dream team</h1>
+        <h2 className="soustitre-apropos">Nos idées</h2>
+      </section>
+      <div className="textes">
+        <p>
+          WeArt nait du besoin de vulgariser l'art grâce à un moyen accessible
+          comme la technologie. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Nesciunt laborum amet omnis! Beatae aliquam cum
+          dolores libero sed quisquam, quae aut asperiores. Ea neque a porro
+          dolores repudiandae quos excepturi. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Amet iusto ipsam excepturi autem, saepe
+          distinctio asperiores error doloremque maxime id. Eos porro magnam eum
+          accusamus quis nostrum et maxime error.
+        </p>
+      </div>
     </>
   );
 }
